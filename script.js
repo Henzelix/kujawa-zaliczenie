f524a2d6a6a1dffb33edfacfbe660d9301df666e
Mikołaj Henzel <50414544+Henzelix@users.noreply.github.com>
2023-10-30 14:18:12 +0000

    fixes

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const currentDateParagraph = document.getElementById("currentDate");
 const currentRatesParagraph = document.getElementById("currentRates");
-let apiResponse, eur, usd, cad;
+let apiResponse = '';
+let eur, usd, cad = 1.50;
+
+const updateTime = () => {
+  const currentDate = new Date();
+  const currentDateTimeString = currentDate.toLocaleString();
+  currentDateParagraph.innerText = currentDateTimeString;
+}
+
+updateTime();
 
 setInterval(function(){
-    console.log("dziala funkcja")
-    const currentDate = new Date();
-    const currentDateTimeString = currentDate.toLocaleString();
-    currentDateParagraph.innerText = currentDateTimeString;
+    updateTime();
 }, 1000);
 
 const apiUrl = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_hg2bXZkgXQu3MJ6HDyCsplvK1NEYjt4NtuUQYevG&currencies=EUR%2CUSD%2CCAD&base_currency=PLN';
@@ -20,28 +26,35 @@ fetch(apiUrl)
   })
   .then(data => {
     // Work with the JSON data returned by the API
-    console.log(data);
     apiResponse = data;
-    console.log(apiResponse['data'])
 
-    eur = apiResponse['data']['EUR']['value'].toFixed(2)
-    usd = apiResponse['data']['USD']['value'].toFixed(2)
-    cad = apiResponse['data']['CAD']['value'].toFixed(2)
+    eur = apiResponse['data']['EUR']['value'].toFixed(2);
+    usd = apiResponse['data']['USD']['value'].toFixed(2);
+    cad = apiResponse['data']['CAD']['value'].toFixed(2);
 
-    currentRatesParagraph.innerText = `1PLN = ${eur}EUR\n1PLN = ${usd}USD\n1PLN = ${cad}CAD`
+    currentRatesParagraph.innerText = `1PLN = ${eur}EUR\n1PLN = ${usd}USD\n1PLN = ${cad}CAD`;
+
+    document.querySelector('.submitButton').disabled = false;
+
   })
   .catch(error => {
     console.error('Error:', error);
   });
 
 
-const przelicz = (event) => {
-    event.preventDefault()
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  przelicz();
+});
+
+const przelicz = () => {
     const inputValue = document.getElementById('currencyInput');
     let afterComma;
     if(inputValue.value.toString().includes('.')){
       afterComma = inputValue.value.toString().split('.')[1];
-      console.log(afterComma);
 
       if (afterComma.length>2){
         currentRatesParagraph.innerText = "niepoprawne dane";
